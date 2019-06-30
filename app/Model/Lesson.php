d2274ab81ed65b6b36198929dc86cdb7edfe169f
kk <403552501@qq.com>
2019-07-01 06:58:32 +0800

    多表关联操作

--- app/Model/Lesson.php
+++ app/Model/Lesson.php
@@ -8,4 +8,8 @@ class Lesson extends Model
 {
     //不允许批量添加的数据
     protected $guarded = [];
+    //与视频模型的一对多关联
+    public function videos(){
+        return $this->hasMany(Video::class);
+    }
 }
